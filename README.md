46758608f79a69249f2d2e57267a31741aa255aa
Stefan <stefanstelzer@me.com>
2020-09-04 14:33:01 +0200

    Save state before continuing development on the mac book.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ionic-projects-app
 Ionic Angular Skill Demo App
+
+**Abstract**
+
+**Technologies**
+
+**How to start the (mobile) Application**
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,6 +7,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
+  
   public folder: string;
 
   constructor(private activatedRoute: ActivatedRoute) { }
